b6ea722a8ea74903ac8807136941bb130642a384
d16sekine <d16sekine@gmail.com>
2018-10-30 09:06:42 +0900

    allow cors

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var routes = require('./api/routes/routes');
 routes(app);
 
